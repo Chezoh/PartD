c2b6b1d156dc5251db615713bae07c01a73c0448
Chezoh <cherylmc77@hotmail.com>
2021-01-16 17:29:06 +1300

    First commit with 'name'

---
+++ Assignment2_PartD/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main(String[] args) {
+		String name = "Cheryl";
+		String colour = "purple";
+		String transport = "bike";
+		String day = "Friday";
+			
+		
+		System.out.println("Hi, my favourite colour is: " + name);
+	}
+}
